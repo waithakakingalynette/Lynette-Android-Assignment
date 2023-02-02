5fe6a5dbebc136575a3ac056f47db34cd8c6ebe7
waithakakingalynette <warukiralynette@gmail.com>
2023-02-02 22:14:15 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    var fullName= "Lynette warukira"
+    var age= 19
+    var phoneNumber: Int= 759826104
+    var weightInKg: Byte= 68
+    var citizen: Boolean= true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weightInKg)
+    println(citizen)
+
+
+
+
+
+
+
+    
+
+}
